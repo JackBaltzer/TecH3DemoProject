b2e050bcf8c569f99da04baf4dc8684a58fc6c55
Jack Baltzer <baltzer@outlook.com>
2021-04-20 18:13:39 +0200

    Add project files.

---
+++ TecH3DemoProject.Api/Domain/Author.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TecH3DemoProject.Api.Domain
+{
+    public class Author
+    {
+
+        public Author()
+        {
+            Books = new List<Book>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(32, ErrorMessage = "Firstname length can't be more than 32 chars")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(32, ErrorMessage = "Lastname length can't be more than 32 chars")]
+        public string LastName { get; set; }
+
+
+        public List<Book> Books { get; set; }
+    }
+}
---
+++ TecH3DemoProject.Api/Domain/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TecH3DemoProject.Api.Domain
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(32, ErrorMessage = "Title length can't be more than 32 chars")]
+        public string Title { get; set; }
+
+        public DateTime Publised { get; set; } = DateTime.Now;
+
+        [ForeignKey("Author.Id")]
+        public int AuthorId { get; set; }
+    }
+}
